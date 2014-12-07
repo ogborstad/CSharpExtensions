fe850bf9fd91c06a1695266a6140767270cf7ced
Ole Gunnar Borstad <ogborstad@gmail.com>
2014-12-07 21:57:58 +0100

    Initial commit. Contains a useful extension on enums decorated as Flags, that gets all the individual flags set in an enum instance.

---
+++ CSharpExtensions.Tests/EnumExtensionsTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace EnumUtilities.Tests
+{
+    [TestFixture]
+    public class EnumExtensionsTests
+    {
+        [Test]
+        public void GetFlags_OneValue_ReturnsValue()
+        {
+            const FlagEnum sampleEnum = FlagEnum.ValueOne;
+            var flags = sampleEnum.GetFlags();
+
+            var enumerable = flags as IList<Enum> ?? flags.ToList();
+            Assert.That(enumerable.Count(), Is.EqualTo(1));
+            Assert.That(enumerable.First(), Is.EqualTo(FlagEnum.ValueOne));
+        }
+
+        [Test]
+        public void GetFlags_TwoValues_ReturnsBothValues()
+        {
+            const FlagEnum sampleEnum = FlagEnum.ValueOne | FlagEnum.ValueTwo;
+            var flags = sampleEnum.GetFlags();
+
+            var enumerable = flags as Enum[] ?? flags.ToArray();
+            Assert.That(enumerable.Count(), Is.EqualTo(2));
+            Assert.That(enumerable.Contains(FlagEnum.ValueOne));
+            Assert.That(enumerable.Contains(FlagEnum.ValueTwo));
+        }
+
+        [Test]
+        public void GetFlagsForEnumWithZeroValue_Zero_ReturnsZero()
+        {
+            const FlagWithZeroEnum sampleEnum = FlagWithZeroEnum.None;
+            var flags = sampleEnum.GetFlags();
+
+            var enumerable = flags as IList<Enum> ?? flags.ToList();
+            Assert.That(enumerable.Count(), Is.EqualTo(1));
+            Assert.That(enumerable.First(), Is.EqualTo(FlagWithZeroEnum.None));
+        }
+
+    }
+
+    [Flags]
+    enum FlagEnum
+    {
+        ValueOne = 1,
+        ValueTwo = 2,
+        ValueThree = 4,
+        ValueFour = 6
+    }
+
+    [Flags]
+    enum FlagWithZeroEnum
+    {
+        None = 0,
+        ValueOne = 1,
+        ValueTwo = 2,
+        ValueThree = 4,
+        ValueFour = 6
+    }
+}
---
+++ CSharpExtensions/EnumExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EnumUtilities
+{
+    public static class EnumExtensions
+    {
+        public static IEnumerable<Enum> GetFlags(this Enum input)
+        {
+            return
+                Enum.GetValues(input.GetType())
+                .Cast<Enum>()
+                .Where(input.HasFlag);
+        }
+    }
+}
